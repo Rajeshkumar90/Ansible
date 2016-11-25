85f73cd5f9469c86d45c20594d86a527063682c1
Rajeshkumar <rajeshkumar27390@gmail.com>
2016-11-25 18:14:16 +0530

    Update win_addpath.ps1

--- lib/ansible/modules/windows/win_addpath.ps1
+++ lib/ansible/modules/windows/win_addpath.ps1
@@ -13,7 +13,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
-
 # WANT_JSON
 # POWERSHELL_COMMON
 $ErrorActionPreference = "Stop"
