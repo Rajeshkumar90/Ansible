2ce8c8d76143f643b3794ed42ea3e1c457a5b2d4
Rajeshkumar <rajeshkumar27390@gmail.com>
2016-11-28 12:59:00 +0530

    Adding win_shortcut py file
    Adding win_shortcut py file

---
+++ lib/ansible/modules/windows/win_shortcut.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# (c) 2016, RajeshKumar SyesShabir
+#
+# This file is part of Ansible
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+# this is a windows documentation stub.  actual code lives in the .ps1
+# file of the same name
+
+DOCUMENTATION = '''
+---
+module: win_shortcut
+version_added: "2.3"
+short_description: Adds Windows shortcut on windows hosts.
+description:
+    - Uses .net Environment to set path environment variables and can set at User, Machine and Process level.  
+options:
+ src:
+    description: 
+      - Path to add windows shortcut.
+    required: true
+    default: no default
+ dest:
+    description: 
+      - The level at which to set the environment variable.
+    required: true
+    default: no default
+author: "Syed, RajeshKumar"
+notes: 
+   - This module is helpful in deploying application for windows shortcut.  
+'''
+
+EXAMPLES = '''
+  # Creates windows shortcut for exe
+  win_shortcut:
+    src: 'C:\Program Files\Mozilla Firefox\Firefox.exe'
+    dest: 'C:\Users\test\Desktop\Firefox.lnk'
+  # Creates windows shortcut for url
+  win_shortcut:
+    src: 'C:\Program Files\Mozilla Firefox\Firefox.exe'
+    dest: 'C:\Users\test\Desktop\Firefox.url'
+'''
+
+RETURN = '''
+'''
